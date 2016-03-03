ece2536712fe7240945859d7f858b0e9f135ce98
Micah Wells <micahp0506@gmail.com>
2016-03-03 14:07:34 -0600

    Working to get chats storing at databse

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-/*eslint-disable */
+/* eslint-disable */
 ;(function () {
   'use strict'
 
--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 
 const express = require('express');
 const app = express();
+const pg = require('pg').native;
 const server = require('http').createServer(app);
 const ws = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
+const POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://localhost:5432/nodechat';
+let db;
 app.set('view engine', 'jade');
 
 app.use(express.static('public'));
@@ -15,9 +18,23 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-server.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`);
-});
+app.get('/chats', (req, res) => {
+    db.query('SELECT * FROM chats', (err, results) => {
+        if (err) throw err
+
+        res.send(results.rows);
+    });
+})
+
+pg.connect(POSTGRES_URL, (err, client) => {
+    if (err) throw err
+
+    db = client;
+
+    server.listen(PORT, () => {
+        console.log(`Server listening on PORT ${PORT}`);
+    });
+})
 
 ws.on('connection', socket => {
     console.log("socket connected");
