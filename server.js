9643749a684d17700bca2caf8b2572f6739af226
Micah Wells <micahp0506@gmail.com>
2016-03-03 09:59:43 -0600

    Basic express server running

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 'use strict';
 
 
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Yo!!');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
+
+
